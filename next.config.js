bb39d3730ddf3f56a1b9519b2755b1e16ec3013b
kelyAna <kely.lopes@vtex.com.br>
2023-01-23 15:44:25 -0300

    chore: fix configs

--- next.config.js
+++ next.config.js
@@ -1,19 +1,16 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 
   images: {
-    domains: [
-      'files.stripe.com'
-    ]
+     domains: ['files.stripe.com'],
   },
 
-  experimental: {
-    images: {
-      unoptimized: true,
-    },
-  }
-}
+  env: {
+     NEXT_URL: process.env.PORT ?? 'http://localhost:3000',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
